0e57f4fcb2dd3fead2005c29ba14513320376e71
Matheus Maciel Leão <matheusmacielleao@gmail.com>
2025-07-04 10:42:56 -0300

    feat: body validation

--- src/application/farmer/dtos/create-farmer.dto.ts
+++ src/application/farmer/dtos/create-farmer.dto.ts
@@ -1,4 +1,12 @@
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class CreateFarmerDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(9)
   documentNumber: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
 }
