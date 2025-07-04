d25d6090dce81e6126a7424cd440a194fc80f0ab
Matheus Maciel Leão <matheusmacielleao@gmail.com>
2025-07-04 12:12:44 -0300

    feat: validation for create farm route

--- src/application/farm/dtos/create-farm.dto.ts
+++ src/application/farm/dtos/create-farm.dto.ts
@@ -1,8 +1,22 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateFarmDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+  @IsString()
+  @IsNotEmpty()
   city: string;
+  @IsString()
+  @IsNotEmpty()
   state: string;
+  @IsNumber()
+  @IsNotEmpty()
   totalAreaHectares: number;
+  @IsNumber()
+  @IsNotEmpty()
   utilAreaHectares: number;
+  @IsNumber()
+  @IsNotEmpty()
   vegetationAreaHectares: number;
 }
